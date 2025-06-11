d7beb0933a68666a6ef50a34747292a402589d7c
memlith <iamcaiosimonassi@gmail.com>
2025-06-11 10:07:05 -0300

    remove versao rodape

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1354,7 +1354,7 @@
                 </main>
 
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    &copy; MySpot 2025. Todos os direitos reservados
                 </footer>
             </div>
         </div>
